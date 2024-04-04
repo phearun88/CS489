da3161ee781ee3f1f3b90b94c5c7c4a650fa3aa6
phearun168 <phin.phearun2013@gmail.com>
2024-04-04 16:04:10 -0500

    upload lab3lab4

---
+++ Assignments/lab3lab4/src/main/java/com/example/lab3lab4/Lab3lab4Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3lab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3lab4Application.class, args);
+	}
+
+}
